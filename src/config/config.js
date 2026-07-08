fa384f87933c98261f523c164b887c312d127721
Educim <eduardolb555@gmail.com>
2026-07-08 12:06:07 -0400

    Delete src directory

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-// Configuración de variables de entorno
-require('dotenv').config();
-
-const config = {
-  PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'development'
-};
-
-module.exports = config;
